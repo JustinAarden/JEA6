43d9baa4aa1178e1a3e3069eb85c3e5359e172fc
Justin <info@justinaarden.nl>
2016-02-22 15:35:34 +0100

    Converted to Maven

--- src/main/java/service/rest/KwetterResource.java
+++ src/main/java/service/rest/KwetterResource.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package service.rest;
 
 import domain.Tweet;
 import domain.User;
-import java.util.List;
-import javax.faces.bean.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import service.KService;
+
+import javax.faces.bean.RequestScoped;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
 
 
 @Path("")
